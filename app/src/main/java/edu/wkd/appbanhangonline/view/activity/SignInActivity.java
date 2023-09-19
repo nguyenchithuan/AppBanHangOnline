fb856a323c8452cdcff05ef9d2e4ec3a914d020c
ngocyeuhuong2805 <ngocyeuhuong280504@gmail.com>
2023-09-20 02:20:10 +0700

    Ngoc_29/9_GiaoDienDangNhapChuaXong

---
+++ app/src/main/java/edu/wkd/appbanhangonline/view/activity/SignInActivity.java
@@ -0,0 +1,16 @@
+package edu.wkd.appbanhangonline.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.wkd.appbanhangonline.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
