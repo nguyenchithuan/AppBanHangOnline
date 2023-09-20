f1720b670fe4edbfa10c8206d767c726a1313d59
ngocyeuhuong2805 <ngocyeuhuong280504@gmail.com>
2023-09-20 22:38:07 +0700

    Ngocnt20/9_XongGiaoDienDangNhap

--- app/src/main/java/edu/wkd/appbanhangonline/view/activity/SignInActivity.java
+++ app/src/main/java/edu/wkd/appbanhangonline/view/activity/SignInActivity.java
@@ -1,16 +1,23 @@
 package edu.wkd.appbanhangonline.view.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wkd.appbanhangonline.R;
 
+
 public class SignInActivity extends AppCompatActivity {
+    private
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sign_in);
+
+
     }
 }
--- app/src/main/java/edu/wkd/appbanhangonline/view/activity/SignUpActivity.java
+++ app/src/main/java/edu/wkd/appbanhangonline/view/activity/SignUpActivity.java
@@ -9,15 +9,15 @@ import edu.wkd.appbanhangonline.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private ActivitySignUpBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
 
-        binding.edEmail.getText();
+
+        setContentView(R.layout.activity_sign_up);
+
+
     }
 }
